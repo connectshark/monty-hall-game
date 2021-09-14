3cdffca8cfb5512df08578986c1549720e5e6797
connectshark <46649021+connectshark@users.noreply.github.com>
2021-09-14 06:44:00 +0000

    Deploying to gh-pages from @ connectshark/monty-hall-game@8674005893a3a5098e4b22fb68fda22382bde761 🚀

---
+++ assets/404.b9e10088.js
@@ -0,0 +1,1 @@
+import{r as a,c as s,b as r,w as n,F as o,a as t,o as e,d as l}from"./vendor.3a1be70a.js";const u={},d=t("h2",null,"無此頁",-1),c=l("回首頁");u.render=function(t,l){const u=a("router-link");return e(),s(o,null,[d,r(u,{to:"/"},{default:n((()=>[c])),_:1})],64)};export{u as default};
